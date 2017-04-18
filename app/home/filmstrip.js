3dd5a19b44a901226783b815353470367f8492a2
Andrew C. Bodine <bodine.andrew@gmail.com>
2017-04-17 22:32:28 -0400

    Hooks up home carousel to Cloudfront for images.

--- app/home/filmstrip.js
+++ app/home/filmstrip.js
@@ -17,116 +17,39 @@ module.controller('FilmstripCtrl', [
   FilmstripCtrl
 ]);
 
-var spotlight;
-
 function FilmstripCtrl ($scope, CloudFront) {
+  $scope.images = [];
 
+  // slide() is a workaround for weird behavior with ng-class in
+  // the home carousel template. Here we simply call the .carousel()
+  // method that is provided by Bootstrap libs.
+  $scope.slide = function (direction) {
+    var action = 'next';
+
+    if(direction === 'prev') action = direction;
+
+    $('#carousel-filmstrip').carousel(action);
+  };
+
+  // Fetch the images under the filmstrip directory in S3 storage, and build
+  // up an array of images that will drive how Angular renders the filmstrip
+  // images to the home carousel.
   CloudFront.get('filmstrip/')
   .then(function (keys) {
-    var images = [];
     var loaded = 0;
 
     for (var i = 0; i < keys.length; i++) {
       var img = new Image();
 
       img.src = encodeURI(CloudFront.baseUrl + '/' + keys[i]);
+      img.alt = keys[i];
 
-      img.onload = function () {
-        loaded = loaded + 1;
-
-        // If this is the last image to load
-        // start the rendering logic
-        if (loaded == keys.length) {
-          setFillerWidth(images);
-
-          // Inject the images into the #filmStrip div
-          for (var j = 0; j < images.length; j++) {
-            $('#filmStrip').append(resizeImage(images[j]));
-          }
-
-          // Calculate an index that is about in the middle of the children array.
-          // This index never changes because we are moving the first and last children
-          // around respectively, and gives us some room to do that out of the user's
-          // sight.
-          spotlight = Math.round($('#filmStrip').children().length / 2);
-
-          centerSpotlight();
-
-          // $scope.$apply();
-        }
-      };
-
-      images.push(img);
+      $scope.images.push(img)
     }
 
-    loopFilmstrip();
+    $scope.$apply();
   })
   .catch(function (err) {
     console.error('Failed to get filmstrip media from CloudFront: ', err);
   });
-
-  function loopFilmstrip () {
-    slide(1);
-
-    setTimeout(function () {
-      loopFilmstrip();
-    }, 4000);
-  }
-}
-
-function resizeImage (image) {
-  var ratio = image.width / image.height;
-
-  image.height = 250;
-  image.width = Math.round(250 * ratio);
-
-  return image;
-}
-
-// This replaces the need to set the #filmStrip width in css
-function setFillerWidth (images) {
-  var total = 0;
-
-  for (var i = 0; i < images.length; i++) {
-    total += images[i].width;
-  }
-
-  $('#filmStrip').width(total);
-}
-
-function slide (direction) {
-  // With jQuery, when you prepend() or append() a child,
-  // if the child is already one of the parent's children,
-  // then jQuery doesn't create another child, it simply
-  // moves it.
-
-  if (direction < 0) {
-    var lastImage = $('#filmStrip').children().last();
-    $('#filmStrip').prepend(lastImage);
-  }
-  else {
-    var firstImage = $('#filmStrip').children().first();
-    $('#filmStrip').append(firstImage);
-  }
-
-  centerSpotlight();
-}
-
-// centerSpotlight() basically calculates what to set the marginLeft
-// property of the #filmStrip div, so that the spotlight image is centered.
-function centerSpotlight () {
-  var widthSum = 0;
-
-  var children = $('#filmStrip').children().slice(0, spotlight + 1);
-
-  for (var i = 0; i < children.length; i++) {
-    if (i == spotlight) {
-      widthSum += Math.round(children[i].width / 2);
-    }
-    else widthSum += children[i].width;
-  }
-
-  var adjustment = widthSum - Math.round($('#viewer').width() / 2);
-
-  $('#filmStrip').animate({'marginLeft': '-' + adjustment}, 0);
 }
