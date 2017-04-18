d7c56576121b035cf57ba91e87a0e70c5abd5f0b
Andrew C. Bodine <bodine.andrew@gmail.com>
2017-04-18 19:30:51 -0400

    Updates to make about board members view work with new S3 structure.

--- app/about/about.js
+++ app/about/about.js
@@ -23,33 +23,72 @@ function stripExtension (name) {
 
 function AboutCtrl ($scope, CloudFront) {
   $scope.members = [];
-  $scope.officers = [];
 
   CloudFront.get('staff/500x500/')
   .then(function (keys) {
 
     for (var i = 0; i < keys.length; i++) {
       var parts = keys[i].split('/').slice(2);
-      var url = encodeURI(CloudFront.baseUrl + '/' + keys[i]);
 
-      if (parts[0] === 'officers') {
-        $scope.officers.push({
-          position: parts[1],
-          name: stripExtension(parts[2]),
-          src: url
-        })
-      }
-      else {
-        $scope.members.push({
-          name: stripExtension(parts[0]),
-          src: url
-        });
-      }
+      if (parts.length <= 1) continue;
+
+      var url = CloudFront.baseUrl + '/' + encodeURI(keys[i]).replace(/\+/g, '%2B');
+      console.log(url);
+
+      var positions = parts[0].split('+');
+
+      positions = positions.map(function (p) {
+        var position = p.replace(/-/g, ' ').split(' ').map(function (i) {
+          return i[0].toUpperCase() + i.slice(1);
+        }).join(' ');
+
+        // This is temporary until we rename things in S3.
+        if (position === 'Board Members') return 'PYTCo Board'
+
+        return position;
+      });
+
+      $scope.members.push({
+        name: stripExtension(parts[1]),
+        src: url,
+        positions: positions
+      });
     }
+
+    $scope.members = $scope.members.sort(sortMembers);
 
     $scope.$apply();
   })
   .catch(function (err) {
     console.error('Failed to get board officer media from CloudFront: ', err);
   });
+}
+
+var hierarchy = [
+    'President',
+    'Vice President',
+    'Treasurer',
+    'Secretary'
+]
+
+function sortMembers (a, b) {
+    var aOffice = -1,
+        bOffice = aOffice;
+
+    for (var i = 0; i < a.positions.length && aOffice == -1; i++) {
+        aOffice = hierarchy.indexOf(a.positions[i]);
+    }
+
+    for (var i = 0; i < b.positions.length && bOffice == -1; i++) {
+        bOffice = hierarchy.indexOf(b.positions[i]);
+    }
+
+    if (aOffice == -1 && bOffice == -1) return 0;
+
+    if (aOffice > -1 && bOffice > -1) {
+        return (aOffice > bOffice) ? 1 : -1;
+    }
+
+    if (aOffice > -1) return -1;
+    return 1;
 }
